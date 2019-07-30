c4129494b5b4923d6f7a86a5a8339a890faf5abb
thomas.bundgaard <thomas.bundgaard@soprasteria.com>
2019-07-30 16:11:39 +0200

    commit

--- src/main/java/com/sopra/MyMain.java
+++ src/main/java/com/sopra/MyMain.java
@@ -2,6 +2,9 @@ package com.sopra;
 public class MyMain {
 
 	public static void main(String[] args) {
-		System.out.println("this is my main");
+		System.out.println(new MyMain().getTheString());
+	}
+	public String getTheString() {
+		return "this is my main";
 	}
 }
---
+++ src/test/java/com/sopra/MyMainTest.java
@@ -0,0 +1,17 @@
+package com.sopra;
+
+import org.junit.jupiter.api.Test;
+
+public class MyMainTest {
+
+	@Test
+	public void testFail() {
+		assert(false);
+	}
+	
+	@Test
+	public void testing() {
+		assert(new MyMain().getTheString().equals("this is my main"));
+	}
+
+}
